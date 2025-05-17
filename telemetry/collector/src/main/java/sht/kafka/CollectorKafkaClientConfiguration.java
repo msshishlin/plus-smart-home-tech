1fb9da64b357e0ff4eee9837ea266b274ff38790
Maksim Shishlin <ms.shishlin@yandex.ru>
2025-05-17 12:20:06 +0300

    Добавил логгер запросов, поправил ошибку сериализации в Avro формат

--- telemetry/collector/src/main/java/sht/kafka/CollectorKafkaClientConfiguration.java
+++ telemetry/collector/src/main/java/sht/kafka/CollectorKafkaClientConfiguration.java
@@ -4,7 +4,7 @@ import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -50,7 +50,7 @@ public class CollectorKafkaClientConfiguration {
             private void initProducer() {
                 Properties config = new Properties();
                 config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-                config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+                config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
                 config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CollectorKafkaAvroSerializer.class);
 
                 producer = new KafkaProducer<>(config);
